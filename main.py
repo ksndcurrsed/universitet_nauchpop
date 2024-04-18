80b6b8c3bc2199e8aa1896fa405731f7c0227ebc
ksndcurrsed <fff1uk6@gmail.com>
2024-04-18 22:29:21 +0300

    v2
    rework code
    selenium 1 part

--- main.py
+++ main.py
@@ -4,68 +4,92 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types.input_file import FSInputFile
-from aiogram.types import InputFile
+from aiogram.types import KeyboardButton, CallbackQuery
 from aiogram import F
-from scripts import parse
+from scripts import univer
 global course
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token="5927699959:AAHAltTvMZRd1pRfAULj6L6J8YQP4ch96lk")
 dp = Dispatcher()
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await bot.send_photo(chat_id=message.chat.id, photo=FSInputFile('photos/logo.png', 'rb'))
-    await message.answer("Привет! Добро пожаловать в бота по упрощению учебной жизни в Финансовом Университете при Правительстве РФ КФ\nЗдесь ты можешь посмотреть свое расписание командой /scheduler," 
-                         "выбрать свой курс командой /course. \nНа данном этапе это не итоговая версия, некоторые функции будут дополняться, следите за обновлениями")
 course = ''
 
+def kb_menu():
+    buttons = [
+        [KeyboardButton(text='Расписание', callback_data= "scheduler")], 
+        [KeyboardButton(text='Выбор курса', callback_data= "course")]
+    ]
 
-@dp.message(Command("course"))
-async def cmd_course(message: types.Message):
-    kb = [
+    keyboard = types.ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+    return keyboard
+
+def kb_course():
+    buttons = [
         [types.KeyboardButton(text="1 курс")],
         [types.KeyboardButton(text="2 курс")],
         [types.KeyboardButton(text="3 курс")],
         [types.KeyboardButton(text="4 курс")]
     ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    await message.answer("Выбери свой курс", reply_markup=keyboard)
+
+    keyboard = types.ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+    return keyboard
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+
+    await bot.send_photo(chat_id=message.chat.id, photo=FSInputFile('photos/logo.png', 'rb'))
+    await message.answer("<b>Привет!🎉</b> Добро пожаловать в бота по упрощению учебной жизни в Финансовом Университете при Правительстве РФ КФ\n\n"
+                        "Здесь ты можешь посмотреть свое расписание командой <code>/scheduler</code>, выбрать свой курс командой <code>/course</code>.\n\n"
+                        "На данном этапе это не итоговая версия, некоторые функции будут дополняться, следите за обновлениями 🚀", reply_markup= kb_menu())
+
+@dp.message(F.text == 'Выбор курса')
+async def cmd_course(message: types.Message):
+    await message.answer("Выбери свой курс", reply_markup=kb_course())
 
 @dp.message(F.text.lower() == "1 курс")
 async def course1(message: types.Message):
     global course 
     course = '1'
-    await message.answer('Отлично, я записал!')
+    await message.answer('Отлично, я записал!', reply_markup= kb_menu())
+
 @dp.message(F.text.lower() == "2 курс")
 async def course2(message: types.Message):
     global course 
     course = '2'
-    await message.answer('Отлично, я записал!')
+    await message.answer('Отлично, я записал!', reply_markup= kb_menu())
+
 @dp.message(F.text.lower() == "3 курс")
 async def course3(message: types.Message):
     global course 
     course = '3'
-    await message.answer('Отлично, я записал!')
+    await message.answer('Отлично, я записал!', reply_markup= kb_menu())
+
 @dp.message(F.text.lower() == "4 курс")
 async def course4(message: types.Message):
     global course 
     course = '4'
-    await message.answer('Отлично, я записал!')
+    await message.answer('Отлично, я записал!', reply_markup= kb_menu())
 
-@dp.message(Command("scheduler"))
+@dp.message(F.text == 'Расписание')
 async def cmd_sheduler(message: types.Message):
-    now = datetime.datetime.now()
     global course
-    response = parse(course).schedule(course)
-    print(response)
+    if course != '':
+        response = univer(course).schedule(course)
+        print(response)
+        document = FSInputFile('расписание.pdf')
+        await bot.send_document(message.chat.id, document, reply_markup= kb_menu())
+    else:
+        await message.answer('Ошибка! Для начала укажите свой курс!', reply_markup= kb_menu())
 
-
-    document = FSInputFile('расписание.pdf')
-    await bot.send_document(message.chat.id, document)
 @dp.message(Command("whatcourse"))
 async def start(message: types.Message):
     await message.answer("Ты учишься на: " + course)
+
+@dp.message(Command('rofls'))
+async def start(message:types.Message):
+    response = univer(course).parsing()
+    await message.answer(response, reply_markup= kb_menu())
+
 async def main():
     await dp.start_polling(bot)
 
--- scripts.py
+++ scripts.py
@@ -1,9 +1,17 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class parse:
+
+url = 'https://org.fa.ru/'
+
+class univer:
     def __init__(self, course: str):
         self.course = course
-    
+        self.url = url
     
     def schedule(self, course):
         linkresp = 'http://www.fa.ru/fil/kaluga/student/Documents/%d0%a0%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5/'+ course + '%20%d0%ba%d1%83%d1%80%d1%81.pdf'
@@ -11,5 +19,20 @@ class parse:
         with open('расписание.pdf', 'wb') as file:
             file.write(response.content)
         return 'сделано'
+    
+    def __setup(self):
+        o = Options()
+        o.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=o)
+        self.__login()
 
-                            
+    def __login(self):
+        self.driver.get(url)
+        self.driver.find_element(By.NAME, 'USER_LOGIN').send_keys('100DOT33SL230303')
+        self.driver.find_element(By.NAME, 'USER_PASSWORD').send_keys('FnOa3304')
+        self.driver.find_element(By.CLASS_NAME, 'login-btn').click()
+
+    def parsing(self):
+        self.__setup()
+        self.driver.get('https://org.fa.ru/app/profile;mode=edu/marks')
+        return self.driver.find_element(By.CLASS_NAME, 'subject').text()
